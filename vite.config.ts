7068ef2f132e6a8c6c4a3c09b973176a5048d11e
Roboticela <team@roboticela.com>
2026-05-23 20:48:49 +0500

    feat: implement SEO enhancements, sitemap generation, and deferred analytics loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://app.osi-model-simulator.roboticela.com',
+      dynamicRoutes: [
+        '/',
+        '/legacy'
+      ]
+    })
   ],
 })
